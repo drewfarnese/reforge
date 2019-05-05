1701442001227209831630e9cf8656d02e554bf3
drewfarnese <50279602+drewfarnese@users.noreply.github.com>
2019-05-05 15:44:09 -0400

    Icon assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import Navbar from './Navbar'
 import Die from './Die'
 import HealthBar from './HealthBar'
